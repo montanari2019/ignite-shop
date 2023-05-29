fb9a805545ed91780b5df28cdafd0bc870b40316
montanari2019~ <ikaro.montanari31@gmail.com>
2023-05-29 15:53:24 -0400

    em andamento

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,11 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function Product(){
 
     const { query } = useRouter();
     return(
-        <div>
-            <h1>Produtos: {JSON.stringify(query)}</h1>
-        </div>
+        <ProductContainer>
+            <ImageContainer>
+                {/* <Image src={}/> */}
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Cmaiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p></p>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
