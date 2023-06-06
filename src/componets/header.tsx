94de11a28f48d2a87bedb60cab656115d02e9c68
montanari2019 <ikaro.montanari31@gmail.com>
2023-06-05 22:25:54 -0400

    implementando icone do carrinho

--- src/componets/header.tsx
+++ src/componets/header.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import logo from "../assets/logo.svg";
-import { Header } from "@/styles/pages/header";
+import logoCarrinho from "../assets/logo-carrinho.svg";
+import {
+  BageInfo,
+  CardAjust,
+  Header,
+  LogoCarrinho,
+} from "@/styles/pages/header";
 
 export default function HeaderComponent() {
   return (
     <Header className="header__logo">
       <Image src={logo} alt="logo" />
+      <CardAjust>
+        <BageInfo>1</BageInfo>
+
+        <LogoCarrinho>
+          <Image src={logoCarrinho} alt="" width={24} height={24} />
+        </LogoCarrinho>
+      </CardAjust>
     </Header>
   );
 }
--- src/styles/pages/header.ts
+++ src/styles/pages/header.ts
@@ -1,8 +1,55 @@
 import { styled } from "@/styles";
 
-export const Header = styled('header',{
-    padding:'2rem 0',
-    width: '100%',
-    maxWidth: 1180,
-    margin: '0 auto'
+export const Header = styled("header", {
+  padding: "2rem 0",
+  width: "100%",
+  maxWidth: 1180,
+  margin: "0 auto",
+  display: "flex",
+  justifyContent: "space-between",
 });
+
+export const LogoCarrinho = styled("div", {
+  width: "3rem",
+  height: "3rem",
+  background: "$gray800",
+
+  borderRadius: 6,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  cursor: 'pointer',
+
+  
+
+  
+});
+
+export const CardAjust = styled('div', {
+    display:'flex',
+    justifyContent: 'flex-end',
+})
+
+
+export const BageInfo = styled('span', {
+   
+    position:'relative',
+    left:'calc(56px - 3px)',
+    top:'-7px',
+    padding:'0.5rem',
+    
+    width: '1.5rem',
+    height: '1.5rem',
+
+    border: 'solid 3px #121214',
+    background: '$green500',
+    borderRadius: '1000px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '0.875rem',
+    fontWeight: 'bold',
+})
